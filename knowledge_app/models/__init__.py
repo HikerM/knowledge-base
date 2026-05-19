c0895d9d25a32975a5107411f45a9916443d05ab
HikerM <masterhiker@icloud.com>
2026-05-19 15:10:24 +0800

    feat: add task queue baseline

--- knowledge_app/models/__init__.py
+++ knowledge_app/models/__init__.py
@@ -4,6 +4,7 @@ from .backup_models import BackupManifest, RestorePlan, SnapshotResult
 from .operation_result import OperationResult
 from .plan_result import PlanResult
 from .search_result import SearchResult
+from .task_models import ProgressEvent, TaskRecord, TaskResult, TaskStatus, TaskType
 from .workspace_status import WorkspaceStatus
 
 __all__ = [
@@ -13,5 +14,10 @@ __all__ = [
     "RestorePlan",
     "SearchResult",
     "SnapshotResult",
+    "ProgressEvent",
+    "TaskRecord",
+    "TaskResult",
+    "TaskStatus",
+    "TaskType",
     "WorkspaceStatus",
 ]
